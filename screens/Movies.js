10a8514cd8b7ce01494390742aa193859cd86b52
고영일 <ykoh728@gmail.com>
2020-08-25 07:45:34 +0900

    sd

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import { movieApi } from '../api';
 
-export default ({ navigation }) => {
+export default () => {
+	const [nowPlaying, setNowPlaying] = useState({
+		movies: [],
+		error: null,
+	});
+	const getData = async () => {
+		const [nowPlaying, error] = await movieApi.nowPlaying();
+		const [popluar, popularError] = await movieApi.popular();
+		console.log(nowPlaying, error);
+	};
+	useEffect(() => {
+		getData();
+	});
 	return (
 		<View style={{ backgroundColor: 'black' }}>
 			<Text>Movies</Text>
-			<Button title="Moive" onPress={() => navigation.navigate('Detail')} />
 		</View>
 	);
 };
