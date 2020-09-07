53c341c1ad8eabafce1f9309699e234edea190cb
kylekoh <ykoh728@gmail.com>
2020-09-07 17:09:22 +0900

    make Detail screen

--- components/HorizontalSlider.js
+++ components/HorizontalSlider.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import { ScrollView } from 'react-native';
-import Title from './Title';
-import PropTypes from 'prop-types';
-import Horizontal from './Horizontal';
+import React from "react";
+import { ScrollView, View } from "react-native";
+import Title from "./Title";
+import PropTypes from "prop-types";
+import Horizontal from "./Horizontal";
 
 const HorizontalSlider = ({ title, children }) => (
-	<>
-		<Title title={title} />
-		<ScrollView
-			style={{ marginTop: 20, marginBottom: 40 }}
-			contentContainerStyle={{ paddingLeft: 30 }}
-			horizontal
-			showsHorizontalScrollIndicator={false}
-		>
-			{children}
-		</ScrollView>
-	</>
+  <View>
+    <Title title={title} />
+    <ScrollView
+      style={{ marginTop: 20, marginBottom: 40 }}
+      contentContainerStyle={{ paddingLeft: 30 }}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+    >
+      {children}
+    </ScrollView>
+  </View>
 );
 
 HorizontalSlider.propTypes = {
-	title: PropTypes.string.isRequired,
-	children: PropTypes.node.isRequired,
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default HorizontalSlider;
