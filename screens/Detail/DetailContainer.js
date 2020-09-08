466fb289cd172cc549e893b5333988990d7196bc
kyle_koh <ykoh728@gmail.com>
2020-09-08 23:15:43 +0900

    add IMDB Link

--- screens/Detail/DetailContainer.js
+++ screens/Detail/DetailContainer.js
@@ -1,49 +1,56 @@
-import React, { useState, useEffect } from "react";
-import DetailPresenter from "./DetailPresenter";
-import { tvApi, movieApi } from "../../api";
+import React, { useState, useEffect } from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import DetailPresenter from './DetailPresenter';
+import { tvApi, movieApi } from '../../api';
 
 export default ({
-  navigation,
-  route: {
-    params: { isTv, id, title, backgroundImage, poster, votes, overview },
-  },
+	navigation,
+	route: {
+		params: { isTv, id, title, backgroundImage, poster, votes, overview },
+	},
 }) => {
-  const [loading, setLoading] = useState(true);
-  const [detail, setDetail] = useState({
-    loading: true,
-    result: {
-      title,
-      backgroundImage,
-      poster,
-      overview,
-      votes,
-    },
-  });
+	const [loading, setLoading] = useState(true);
+	const [detail, setDetail] = useState({
+		loading: true,
+		result: {
+			title,
+			backgroundImage,
+			poster,
+			overview,
+			votes,
+		},
+	});
 
-  const getData = async () => {
-    const [getDetail, getDetailError] = isTv
-      ? await tvApi.show(id)
-      : await movieApi.movie(id);
-    setDetail({
-      loading: false,
-      result: {
-        ...getDetail,
-        title: getDetail.title || getDetail.name,
-        backgroundImage: getDetail.backdrop_path,
-        poster: getDetail.poster_path,
-        overview: getDetail.overview,
-        votes: getDetail.vote_average,
-      },
-    });
-  };
+	const getData = async () => {
+		const data = await tvApi.show(id);
+		const data2 = await movieApi.movie(id);
 
-  useEffect(() => {
-    getData();
-  }, [id]);
+		const [getDetail, getDetailError] = isTv
+			? await tvApi.show(id)
+			: await movieApi.movie(id);
+		setDetail({
+			loading: false,
+			result: {
+				...getDetail,
+				title: getDetail.title || getDetail.name,
+				backgroundImage: getDetail.backdrop_path,
+				poster: getDetail.poster_path,
+				overview: getDetail.overview,
+				votes: getDetail.vote_average,
+			},
+		});
+	};
 
-  React.useLayoutEffect(() => {
-    navigation.setOptions({ title });
-  });
+	useEffect(() => {
+		getData();
+	}, [id]);
 
-  return <DetailPresenter {...detail} />;
+	React.useLayoutEffect(() => {
+		navigation.setOptions({ title });
+	});
+
+	const openBrowser = async (url) => {
+		await WebBrowser.openBrowserAsync(url);
+	};
+	return <DetailPresenter openBrowser={openBrowser} {...detail} />;
 };
--- screens/Detail/DetailPresenter.js
+++ screens/Detail/DetailPresenter.js
@@ -1,85 +1,113 @@
-import React from "react";
-import { View, Text } from "react-native";
-import styled from "styled-components/native";
-import ScrollContainer from "../../components/ScrollContainer";
-import { apiImage } from "../../api";
-import { Dimensions, ActivityIndicator } from "react-native";
-import Poster from "../../components/Poster";
-import Votes from "../../components/Votes";
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import styled from 'styled-components/native';
+import ScrollContainer from '../../components/ScrollContainer';
+import { apiImage } from '../../api';
+import { Dimensions, ActivityIndicator } from 'react-native';
+import Poster from '../../components/Poster';
+import Votes from '../../components/Votes';
+import { formatDate } from '../../utils';
+import Link from '../../components/Detail/Link';
 
 const BG = styled.Image`
-  width: 100%;
-  height: 100%;
-  opacity: 0.4;
-  position: absolute;
+	width: 100%;
+	height: 100%;
+	opacity: 0.4;
+	position: absolute;
 `;
 
 const Header = styled.View`
-  height: ${Dimensions.get("window").height / 3}px;
-  align-items: center;
-  justify-content: flex-end;
+	height: ${Dimensions.get('window').height / 3}px;
+	align-items: center;
+	justify-content: flex-end;
 `;
 
 const Container = styled.View`
-  flex-direction: row;
-  align-items: center;
-  top: 30px;
+	flex-direction: row;
+	align-items: center;
+	top: 30px;
 `;
 
 const Info = styled.View`
-  width: 50%;
-  margin-left: 40px;
+	width: 50%;
+	margin-left: 40px;
 `;
 
 const Title = styled.Text`
-  color: white;
-  font-weight: 600;
-  font-size: 24px;
-  margin-bottom: 10px;
+	color: white;
+	font-weight: 600;
+	font-size: 24px;
+	margin-bottom: 10px;
 `;
 
 const Data = styled.View`
-  margin-top: 30px;
-  padding: 0px 30px;
+	margin-top: 30px;
+	padding: 0px 30px;
 `;
 
 const DataName = styled.Text`
-  color: white;
-  opacity: 0.8;
-  font-weight: 800;
-  margin-bottom: 15px;
+	margin-top: 30px;
+	color: white;
+	opacity: 0.8;
+	font-weight: 800;
+	margin-bottom: 15px;
 `;
 
 const DataValue = styled.Text`
-  color: white;
-  opacity: 0.8;
-  font-weight: 500;
+	color: white;
+	opacity: 0.8;
+	font-weight: 500;
 `;
 
-export default ({ result, loading }) => (
-  <ScrollContainer loading={false}>
-    <>
-      <Header>
-        <BG source={{ uri: apiImage(result.backgroundImage, "-") }} />
-        <Container>
-          <Poster url={result.poster} />
-          <Info>
-            <Title>{result.title}</Title>
-            {result.votes && <Votes votes={result.votes} />}
-          </Info>
-        </Container>
-      </Header>
-      <Data>
-        {result.overview && (
-          <>
-            <DataName>Overview</DataName>
-            <DataValue>{result.overview}</DataValue>
-          </>
-        )}
-        {loading && (
-          <ActivityIndicator styel={{ marginTop: 30 }} color={"white"} />
-        )}
-      </Data>
-    </>
-  </ScrollContainer>
+export default ({ openBrowser, result, loading }) => (
+	<ScrollContainer loading={false}>
+		<>
+			<Header>
+				<BG source={{ uri: apiImage(result.backgroundImage, '-') }} />
+				<Container>
+					<Poster url={result.poster} />
+					<Info>
+						<Title>{result.title}</Title>
+						{result.votes && <Votes votes={result.votes} />}
+					</Info>
+				</Container>
+			</Header>
+			<Data>
+				{result.overview && (
+					<>
+						<DataName>Overview</DataName>
+						<DataValue>{result.overview}</DataValue>
+					</>
+				)}
+				{loading && (
+					<ActivityIndicator style={{ marginTop: 30 }} color={'white'} />
+				)}
+				{result.spoken_languages && (
+					<>
+						<DataName>Languages</DataName>
+						<DataValue>
+							{result.spoken_languages.map((l) => `${l.name} `)}
+						</DataValue>
+					</>
+				)}
+				{result.release_date && (
+					<>
+						<DataName>Release Date</DataName>
+						<DataValue>{formatDate(result.release_date)}</DataValue>
+					</>
+				)}
+				{result.imdb_id && (
+					<TouchableOpacity>
+						<Link
+							text={'IMDB Page'}
+							icon={'imdb'}
+							onPress={() =>
+								openBrowser(`https://www.imdb.com/title/${result.imdb_id}`)
+							}
+						></Link>
+					</TouchableOpacity>
+				)}
+			</Data>
+		</>
+	</ScrollContainer>
 );
