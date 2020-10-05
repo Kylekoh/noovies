0390378147a63ff8642559701eb1ec372a149e0d
kyle_koh <ykoh728@gmail.com>
2020-10-05 22:32:38 +0900

    make interpolate animation

--- screens/Favs/FavsPresenter.js
+++ screens/Favs/FavsPresenter.js
@@ -1,80 +1,118 @@
-import React, { useState } from "react";
-import { View, Text, Dimensions, PanResponder, Animated } from "react-native";
-import styled from "styled-components/native";
-import { apiImage } from "../../api";
+import React, { useState } from 'react';
+import { View, Text, Dimensions, PanResponder, Animated } from 'react-native';
+import styled from 'styled-components/native';
+import { apiImage } from '../../api';
 
-const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
+const { width: WIDTH, height: HEIGHT } = Dimensions.get('window');
 
 const Container = styled.View`
-  flex: 1;
-  background-color: black;
-  align-items: center;
+	flex: 1;
+	background-color: black;
+	align-items: center;
 `;
 
 const Poster = styled.Image`
-  overflow: hidden;
-  border-radius: 20px;
-  width: 100%;
-  height: 100%;
+	overflow: hidden;
+	border-radius: 20px;
+	width: 100%;
+	height: 100%;
 `;
 
 const styles = {
-  top: 80,
-  height: HEIGHT / 1.5,
-  width: "90%",
-  position: "absolute",
+	top: 80,
+	height: HEIGHT / 1.5,
+	width: '90%',
+	position: 'absolute',
 };
 
 export default ({ results }) => {
-  const [topIndex, setTopIndex] = useState(0);
-  const position = new Animated.ValueXY();
-  const panResponder = PanResponder.create({
-    onStartShouldSetPanResponder: () => true,
-    onPanResponderMove: (evt, { dx, dy }) => {
-      position.setValue({ x: dx, y: dy });
-    },
-    onPanResponderRelease: () => {
-      Animated.spring(position, {
-        toValue: {
-          x: 0,
-          y: 0,
-        },
-        bounciness: 28,
-      }).start();
-    },
-  });
+	const [topIndex, setTopIndex] = useState(0);
+	const position = new Animated.ValueXY();
+	const panResponder = PanResponder.create({
+		onStartShouldSetPanResponder: () => true,
+		onPanResponderMove: (evt, { dx, dy }) => {
+			position.setValue({ x: dx, y: dy });
+		},
+		onPanResponderRelease: () => {
+			Animated.spring(position, {
+				toValue: {
+					x: 0,
+					y: 0,
+				},
+				bounciness: 28,
+			}).start();
+		},
+	});
 
-  return (
-    <Container>
-      {results.reverse().map((result, index) => {
-        if (index === topIndex) {
-          return (
-            <Animated.View
-              style={{
-                ...styles,
-                zIndex: 1,
-                transform: [...position.getTranslateTransform()],
-              }}
-              key={result.id}
-              {...panResponder.panHandlers}
-            >
-              <Poster source={{ uri: apiImage(result.poster_path) }} />
-            </Animated.View>
-          );
-        }
-        return (
-          <Animated.View
-            style={{
-              ...styles,
-              transform: [...position.getTranslateTransform()],
-            }}
-            key={result.id}
-            {...panResponder.panHandlers}
-          >
-            <Poster source={{ uri: apiImage(result.poster_path) }} />
-          </Animated.View>
-        );
-      })}
-    </Container>
-  );
+	const rotationValues = position.x.interpolate({
+		inputRange: [-200, 0, 200],
+		outputRange: ['-50deg', '0deg', '5deg'],
+		extrapolate: 'clamp',
+	});
+
+	const secondCartOpacity = position.x.interpolate({
+		inputRange: [-255, 0, 255],
+		outputRange: [1, 0.5, 1],
+		extrapolate: 'clamp',
+	});
+
+	const secondCardScale = position.x.interpolate({
+		inputRange: [-255, 0, 255],
+		outputRange: [1, 0.8, 1],
+		extrapolate: 'clamp',
+	});
+
+	return (
+		<Container>
+			{results.map((result, index) => {
+				if (index === topIndex) {
+					return (
+						<Animated.View
+							style={{
+								...styles,
+								zIndex: 1,
+								transform: [
+									{ rotate: rotationValues },
+									...position.getTranslateTransform(),
+								],
+							}}
+							key={result.id}
+							{...panResponder.panHandlers}
+						>
+							<Poster source={{ uri: apiImage(result.poster_path) }} />
+						</Animated.View>
+					);
+				} else if (index === topIndex + 1) {
+					return (
+						<Animated.View
+							style={{
+								...styles,
+								zIndex: -index,
+								opacity: secondCartOpacity,
+								transform: [{ scale: secondCardScale }],
+							}}
+							key={result.id}
+							{...panResponder.panHandlers}
+						>
+							<Poster source={{ uri: apiImage(result.poster_path) }} />
+						</Animated.View>
+					);
+				} else {
+					return (
+						<Animated.View
+							style={{
+								...styles,
+								zIndex: -index,
+								opacity: 0,
+							}}
+							key={result.id}
+							{...panResponder.panHandlers}
+						>
+							<Poster source={{ uri: apiImage(result.poster_path) }} />
+						</Animated.View>
+					);
+				}
+			})}
+		</Container>
+	);
 };
